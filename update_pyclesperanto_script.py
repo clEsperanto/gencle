ce588bb9f897fab42fea1ce9e22746718912829c
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-08-22 17:20:33 +0200

    clean up script

--- update_pyclesperanto_script.py
+++ update_pyclesperanto_script.py
@@ -1,5 +1,5 @@
 import gencle
-import glob, os, re
+import os
 
 # manage folder paths
 PYCLE_FOLDER = '/data/clesperanto/pyclesperanto'
@@ -8,12 +8,6 @@ PYTHON_FOLDER = os.path.join(PYCLE_FOLDER, 'pyclesperanto')
 
 code_list, tier_list = gencle.read_tier_from_github(repo='clEsperanto/CLIc', branch='master')
 print(f"gencle: Found {len(code_list)} tier files: {tier_list}")
-
-# functions_list = gencle.parse_doxygen_to_json(code_list[0])
-# print(f"gencle: Found {len(functions_list)} functions in tier{tier_list[0]}")
-# print(functions_list[0])
-# wrapper_file = gencle.generate_wrapper_file(functions_list, 1)
-# python_file = gencle.generate_python_file(functions_list, 1)
 
 for tier, code in zip(tier_list, code_list):
     print(f"gencle: Generating tier{tier} files ...")
