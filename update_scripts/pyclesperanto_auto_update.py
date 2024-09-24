f0dc593a2fd6e2fa16eab6e6a4e81dda120f6016
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-24 12:52:10 +0300

    add msg

--- update_scripts/pyclesperanto_auto_update.py
+++ update_scripts/pyclesperanto_auto_update.py
@@ -5,10 +5,11 @@ OUTPUT_REPO = sys.argv[1]
 VERSION_TAG = sys.argv[2]
 SOURCE_REPO = "clEsperanto/CLIc"
 
-def update_tier_code(dst_repo:str, src_repo:str, tag:str):
+
+def update_tier_code(dst_repo: str, src_repo: str, tag: str):
     """
     Update the tier code in the OUTPUT_REPO by reading the tier files from the SOURCE_REPO
-    
+
     Parameters
     ----------
     dst_repo : str
@@ -17,7 +18,7 @@ def update_tier_code(dst_repo:str, src_repo:str, tag:str):
         Path to the SOURCE_REPO folder.
     tag : str
         Version tag to be used in the OUTPUT_REPO.
-        
+
     Returns
     -------
         None
@@ -28,8 +29,12 @@ def update_tier_code(dst_repo:str, src_repo:str, tag:str):
         functions_list = gencle.parse_doxygen_to_json(code)
         wrapper_file = gencle.generate_wrapper_file(functions_list, tier)
         python_file = gencle.generate_python_file(functions_list, tier)
-        wrapper_filepath = os.path.join(os.path.join(dst_repo, 'src/wrapper'), f'tier{tier}_.cpp')
-        python_filepath = os.path.join(os.path.join(dst_repo, 'pyclesperanto'), f'_tier{tier}.py')
+        wrapper_filepath = os.path.join(
+            os.path.join(dst_repo, "src/wrapper"), f"tier{tier}_.cpp"
+        )
+        python_filepath = os.path.join(
+            os.path.join(dst_repo, "pyclesperanto"), f"_tier{tier}.py"
+        )
         gencle.write_file(wrapper_filepath, wrapper_file, overwrite=True)
         gencle.write_file(python_filepath, python_file, overwrite=True)
 
@@ -44,27 +49,29 @@ def update_version_file(dst_repo: str, tag: str):
         Path to the OUTPUT_REPO folder.
     tag : str
         Version tag to be used in the OUTPUT_REPO.
-    
+
     Returns
     -------
         None
     """
 
-    version_file = os.path.join(os.path.join(dst_repo, 'pyclesperanto'), '_version.py')
+    version_file = os.path.join(os.path.join(dst_repo, "pyclesperanto"), "_version.py")
 
     # if file does not exist, return
     if not os.path.exists(version_file):
         print(f"gencle: Fail updating CLIc version. Could not find {version_file}")
         return
 
-    with open(version_file, 'r') as file:
+    with open(version_file, "r") as file:
         data = file.readlines()
     for i, line in enumerate(data):
-        if 'CLIC_VERSION =' in line:
+        if "CLIC_VERSION =" in line:
             data[i] = f'CLIC_VERSION = "{tag}"\n'
             break
-    with open(version_file, 'w') as file:
+    with open(version_file, "w") as file:
         file.writelines(data)
+
+    print(f"gencle: Updated CLIc version tag to '{tag}'")
 
 
 def main():
